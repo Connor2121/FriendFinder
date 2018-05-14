67feb6a3d33a1557c9624878f1949f39635f9766
Connor Reed <connorreed@connors-mbp-2.home>
2018-05-14 14:32:11 -0400

    typo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 var PORT = process.env.PORT || 3000;
 
